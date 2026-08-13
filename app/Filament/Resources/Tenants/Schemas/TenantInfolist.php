98ad858f2291f2bc248466435cc1011695964cbc
kemal <kemalyen@gmail.com>
2026-08-13 20:12:24 +0100

    quick fix for domain

--- app/Filament/Resources/Tenants/Schemas/TenantInfolist.php
+++ app/Filament/Resources/Tenants/Schemas/TenantInfolist.php
@@ -73,7 +73,7 @@ class TenantInfolist
                                     $ssl = 'https://';
                                 }
 
-                                return redirect("{$ssl}{$tenant_domain}.{$domain}impersonate/{$token->token}");
+                                return redirect("{$ssl}{$tenant_domain}/impersonate/{$token->token}");
                             })
                     ]),
                 ])->columns(2),
