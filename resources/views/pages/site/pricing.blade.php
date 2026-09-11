1a55ee1379146664476aa0f01e33c58c83d0eaf2
kemal <kemalyen@gmail.com>
2026-09-11 15:40:02 +0100

    pricing is updated

--- resources/views/pages/site/pricing.blade.php
+++ resources/views/pages/site/pricing.blade.php
@@ -112,15 +112,7 @@ new #[Layout('layouts.frontend')]  class extends Component {};
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M5 13l4 4L19 7" />
                             </svg>
-                            500 registrations per event
-                        </li>
-                        <li class="flex items-start gap-2">
-                            <svg class="w-5 h-5 text-blue-200 flex-shrink-0 mt-0.5" fill="none"
-                                stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M5 13l4 4L19 7" />
-                            </svg>
-                            Custom booking page
+                            Unlimited registrations
                         </li>
                         <li class="flex items-start gap-2">
                             <svg class="w-5 h-5 text-blue-200 flex-shrink-0 mt-0.5" fill="none"
